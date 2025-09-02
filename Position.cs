0c2c45811c6374222fc34787e06b60f367d0ae9a
Joshua Blignaut <ST10472349@vgconnect.edu.za>
2025-09-02 17:10:27 +0200

    Add project files.

---
+++ Position.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6122
+{
+    public class Position
+    {
+        private int _x;
+        private int _y;
+
+        public Position(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public int X
+        {
+            get => _x;
+            set => _x = value;
+        }
+
+        public int Y
+        {
+            get => _y;
+            set => _y = value;
+        }
+    }
+}
